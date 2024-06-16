c42183f199284a94e949daa1c60e6b198cab74da
itsSaishriya <saishriya30@gmail.com>
2024-06-17 01:29:11 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+const accId = 258258;
+let accName = "Acchu";
+var accEmail = "acc@gmail.com";
+accCity = "goa"
+
+// accId = "111111"; // not allowed
+accName = "reddy";
+accEmail = "hello@l.com";
+accCity = "banglore";
+
+console.log(accId);
+console.table([accName, accEmail, accCity, accId]);
+
+/*
+prefer not to use var 
+Becouse of issue on block scope and functional scope 
+*/
